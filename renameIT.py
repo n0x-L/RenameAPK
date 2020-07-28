ce0e850f05b18b9cb0144a116cf2711395939319
LeeLane <leeson10an@gmail.com>
2020-07-28 12:12:09 -0600

    Added new method to search for potential guesses based on number of strings and then whether the strings match. First searching for no of strings in order to try and reduce the complexity

--- renameIT.py
+++ renameIT.py
@@ -1,4 +1,4 @@
-import os, subprocess, sys, argparse, re
+import os, subprocess, sys, argparse, re, collections
 
 # Get a list of file names
 def get_fileNames(aDirectory):
@@ -22,49 +22,78 @@ def get_fileNames(aDirectory):
 		sys.exit()	
 
 
-# Find which files contain strings in named directory
+# Find which files contain strings in named directory and return appropriate values
 def get_StringLists(aFileList, aDirectory):
 	hasStrings = []
 	noStrings = []
 	index = 0
+	strings_chainMap = collections.ChainMap()
+
 	for x in aFileList:
-		tmpString = aDirectory + "/" + x
-		b = subprocess.Popen('grep -a "const-string" %s' % tmpString, stdout=subprocess.PIPE, shell=True)
-		outputTemp, errorTemp = b.communicate()
-		outputTemp = outputTemp.decode('utf-8')
+		tmpDirectory = aDirectory + "/" + x
+		b = subprocess.Popen('grep -a "const-string" %s' % tmpDirectory, stdout=subprocess.PIPE, shell=True)
+		strings, errorTemp = b.communicate()
+		strings = strings.decode('utf-8')
 	
 		# If no strings in file
-		if not outputTemp:
+		if not strings:
 			#print("No strings in file: ", x)
 			noStrings.append(x)
 		else:
 			#print("Strings in file:  ", x)
 			hasStrings.append(x)
-			
-			# Create dictionary item
-			
+
+			# Put strings into a list
+			strings = strings.splitlines()
+
+			tmpDict = {
+			"Filename": x,
+			"no_of_strings": len(strings),
+			"new_name" : "unknown",
+			"strings": strings
+			}
+	
+			#print(tmpDict["strings"][0])
+			strings_chainMap = strings_chainMap.new_child(tmpDict)
+			#chain2 = strings_chainMap.new_child(tmpDict)
+
 
 		# Reset temp string
-		tmpString = ""
+		tmpDirectory = ""
 	
-	return hasStrings, noStrings
+	return hasStrings, noStrings, strings_chainMap
 
-# Print output
-#print("The following files contain strings:")
-#print(hasStrings)
+# Try to find string matches
 
-#print("The following file do not contain any strings:")
-#print(noStrings)
+def string_Matching(unnamed_strings_chain, named_strings_chain):
+	i = 0
+	guesses = []
 
+	for x in unnamed_strings_chain.maps:
+		if x.get('Filename') != None:
+			for y in named_strings_chain.maps:
+				if x.get('no_of_strings') == y.get('no_of_strings'):
+					guesses.append(y)
 
-# Start finding matches for the hasStrings list
-#for x in hasStrings:
-#	c = subprocess.Popen('grep -a "const-string" instantcoffee_named/%s.smali' % x, stdout=subprocess.PIPE, shell=True)
-#	outputTempC, errorTempC = c.communicate()
-#	outputTempC = outputTempC.decode('utf-8')
-#	outputTempC = outputTempC.splitlines()
+			for z in guesses:
+				tmpX = x.get('strings')
+				tmpZ = z.get('strings')
 
+				if tmpX == tmpZ:
+					print('Guess is that ' + x.get('Filename') + " is " + z.get('Filename'))
+					break
 
+					# send guesses to confirm method
+					
+
+			#print("Guesses for " + x.get('Filename'))
+			#print(guesses)
+
+			# Reset list
+			#guesses = []
+
+def confirm_Matches(guesses):
+	# TO-DO
 
 def main():
 	#aDirectory = ""
@@ -73,15 +102,11 @@ def main():
 	namedFileList = get_fileNames('instantcoffee_named')
 	unnamedFileList = get_fileNames('instantcoffee_unnamed')
 	
-	namedHasStrings, namedNoStrings = get_StringLists(namedFileList, 'instantcoffee_named')
-	unnamedHasStrings, unnamedNoStrings = get_StringLists(unnamedFileList, 'instantcoffee_unnamed')
-
-	file1 = {
-		"Name": namedHasStrings[0],
-		"no_of_strings": len(namedHasStrings[0])
-		}
+	namedHasStrings, namedNoStrings, named_strings_chain = get_StringLists(namedFileList, 'instantcoffee_named')
+	unnamedHasStrings, unnamedNoStrings, unnamed_strings_chain = get_StringLists(unnamedFileList, 'instantcoffee_unnamed')
 	
-	print(file1)
+	string_Matching(unnamed_strings_chain, named_strings_chain)
+	#print(named_strings_chain.maps)
 
 
 if __name__ == "__main__":
