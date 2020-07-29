7231bba9e923f29ca7ed9a30538c47c446cb79b9
LeeLane <leeson10an@gmail.com>
2020-07-29 15:49:35 -0600

    Modified initial get_StringLists method to extract strings with regex rather than only doing it with the unique strings in the unique_StringFinder methods, this made matching better in the finalStringGuesses result. Finished the unique_StringMatching method to use those files with strings unique to the entire library in order to make a match up. It evaluates a percentage of alike-ness between each of the unique string library lists in order to determine if it will make the guess or not. This seemed to work very well.

--- renameIT.py
+++ renameIT.py
@@ -24,6 +24,9 @@ def get_fileNames(aDirectory):
 		print("exiting..")
 		sys.exit()	
 
+	# TO-DO
+	# Check that all given filenames are unique!
+
 
 # Find which files contain strings in named directory and return appropriate values
 def get_StringLists(aFileList, aDirectory):
@@ -46,15 +49,18 @@ def get_StringLists(aFileList, aDirectory):
 			#print("Strings in file:  ", x)
 			hasStrings.append(x)
 
+
+
 			# Put strings into a list
-			strings = strings.splitlines()
+			#strings = strings.splitlines()
+			stringsList = re.findall('"([^"]*)"', strings) 
 
 			tmpDict = {
 			"Filename": x,
-			"no_of_strings": len(strings),
+			"no_of_strings": len(stringsList),
 			"new_name" : "unknown",
 			"guessed_name" : "unknown",
-			"strings": strings,
+			"strings": stringsList,
 			"unique_file_strings" : [],
 			"unique_lib_strings" : []
 			}
@@ -70,22 +76,24 @@ def get_StringLists(aFileList, aDirectory):
 	return hasStrings, noStrings, strings_chainMap
 
 # Use precense of unique library strings to make preliminary guesses
-def unique_StringFinder(unnamed_strings_chain, named_strings_chain):
+def unique_StringFinder(strings_chain):
 	tmpString = ""
 	total_unique_fStrings = []
 
 	# For each unnamed file extract the strings unique to that file
-	for x in unnamed_strings_chain.maps:
+	for x in strings_chain.maps:
 		if x.get('Filename') != None:
 			fileStringList = x.get('strings')
 
 			# for each string in the file
-			for aString in fileStringList:
-				tmpString = tmpString + aString # concatenate together in order to use regex to pull out the strings
+			#for aString in fileStringList:
+			#	tmpString = tmpString + aString # concatenate together in order to use regex to pull out the strings
 
-			tmpList = re.findall('"([^"]*)"', tmpString) # pull out the strings from the file into a list
-			occurencesUnnamed = collections.Counter(tmpList) # count the occurences of each string in the file
-			tmpString = ""
+			#tmpList = re.findall('"([^"]*)"', tmpString) # pull out the strings from the file into a list
+			#occurencesUnnamed = collections.Counter(tmpList) # count the occurences of each string in the file
+			#tmpString = ""
+
+			occurencesUnnamed = collections.Counter(fileStringList) 
 
 			# Check the occurences of the string in the file, if it is only once then add it to the list
 			# of unique file strings
@@ -103,13 +111,14 @@ def unique_StringFinder(unnamed_strings_chain, named_strings_chain):
 			#print(x.get('Filename') + ' unique_lib_strings: ')
 			#print(x.get('unique_lib_strings'))
 
-	return unnamed_strings_chain
+	return strings_chain
 
 
 # Try to find file matches based off of string matches
 def string_Matching(unnamed_strings_chain, named_strings_chain):
 	i = 0
 	guesses = []
+	still_unknown = collections.ChainMap()
 
 	for x in unnamed_strings_chain.maps:
 		if x.get('Filename') != None:
@@ -143,9 +152,50 @@ def string_Matching(unnamed_strings_chain, named_strings_chain):
 	
 			# Reset list
 			guesses = []
+			if x.get('guessed_name') == 'unknown':
+				uniqueLibStrings = x.get('unique_lib_strings')
+				if uniqueLibStrings:
+					#still_unknown.append(x)
+					still_unknown = still_unknown.new_child(x)
 
+	return unnamed_strings_chain, still_unknown
+
+# Given a list of still currently unknown files with unique library strings for each file,
+# try and determine based on the unique library string which file it likely is.
+def unique_StringMatching(still_unknown, named_strings_chain, unnamed_strings_chain):
+	for uFile in still_unknown.maps:
+		if uFile.get('Filename') != None:
+			uList = uFile.get('unique_lib_strings')
+
+			#if uFile.get('Filename') == 'cm.smali':
+			#	print('*******cm.smali unique strings: ')
+			#	print(uFile.get('unique_lib_strings'))
+
+			# Iterate over the named files to find a match
+			for nFile in named_strings_chain.maps:
+				if nFile.get('Filename') != None:
+					nList = nFile.get('unique_lib_strings')
+
+					# Generate a percentage value of how similar the two lists are
+					evaluate = len(set(uList) & set(nList)) / float(len(set(uList) | set(nList))) * 100
+
+					# Iterate over the unnamed files and assign 
+					# guesses or matches based on how close the matches are
+					# for the unique library strings list
+					for oFile in unnamed_strings_chain.maps:
+						if oFile.get('Filename') == uFile.get('Filename'):
+							if evaluate == 100.0:
+								oFile.update(guessed_name = nFile.get('Filename')) # if we have a new name then we have a guessed name
+								oFile.update(new_name = nFile.get('Filename'))
+							elif evaluate > 50.0:
+								oFile.update(guessed_name = nFile.get('Filename'))
+							else:
+								continue
+						
 	return unnamed_strings_chain
-		
+								
+
+
 
 #def confirm_Matches(guesses):
 	# TO-DO
@@ -160,15 +210,62 @@ def main():
 	namedHasStrings, namedNoStrings, named_strings_chain = get_StringLists(namedFileList, 'library_named')
 	unnamedHasStrings, unnamedNoStrings, unnamed_strings_chain = get_StringLists(unnamedFileList, 'library_unnamed')
 	
-	unnamed_stringUnique_chain = unique_StringFinder(unnamed_strings_chain, named_strings_chain)
+	# Fill out the 'unique_file_strings' and 'unique_lib_strings' portion of the data structure
+	unnamed_uniqueStringsChain = unique_StringFinder(unnamed_strings_chain)
+	named_uniqueStringsChain = unique_StringFinder(named_strings_chain)
 
-	
-	unnamed_stringMatches_chain = string_Matching(unnamed_stringUnique_chain, named_strings_chain)
+	# Make some matches and get back any that couldn't be matched yet
+	unnamed_stringMatchesChain, still_unknown = string_Matching(unnamed_uniqueStringsChain, named_uniqueStringsChain)
 
-	pprint.pprint(unnamed_stringMatches_chain)
+	print('\n---- Progress Update ---')
+	all_unnamed_stringfilesCount = len(unnamedHasStrings)
+	unknownCount = len(still_unknown.maps)
+	knownCount = all_unnamed_stringfilesCount - unknownCount
+	print('Total number of unnamed files: ' + str(all_unnamed_stringfilesCount))
+	print('Number of (unconfirmed) guesses for files containing strings: ' + str(knownCount))
+	print('Number of unknowns for files containing unique library strings: ' + str(unknownCount))
+	print('\n continuing...')
 
-	#for x in unnamed_stringUnique_chain.maps:
-	#	pprint.pprint(x)
+	finalStringGuesses = unique_StringMatching(still_unknown, named_uniqueStringsChain, unnamed_stringMatchesChain)
+
+	unknownCount = 0
+	knownCount = 0
+	print('\n--GUESSES--')
+	for x in finalStringGuesses.maps:
+		if x.get('Filename') != None:
+			if x.get('guessed_name') != 'unknown':
+				print(x.get('Filename') + ' is ' + x.get('guessed_name'))
+				knownCount+=1
+			else:
+				print('Still not sure who "' + x.get('Filename') + '" is.')
+				unknownCount+=1
+
+	print('\n---- Progress Update ---')
+	totalCount_unnamed = len(unnamedHasStrings) + len(unnamedNoStrings)
+	print('Total number of unnamed files: ' + str(totalCount_unnamed))
+	print('Total number of unnamed files containing strings: ' + str(len(unnamedHasStrings)))
+	print('Number of (unconfirmed) guesses for files containing strings: ' + str(knownCount))
+	print('Number of still unknowns for files containing unique library strings: ' + str(unknownCount))
+	print('\n continuing...')
+
+	#uCount = 0
+	#uuCount = 0
+	#for x in unnamed_uniqueStringsChain.maps:
+	#	if x.get('Filename') != None:
+	#		if x.get('guessed_name') == 'unknown':
+	#			if x.get('unique_lib_strings'):
+	#				uuCount+=1
+	#			print('Not sure who ' + x.get('Filename') + ' is.')
+	#			uCount+=1
+	#print('Number of unknowns counted: ' + str(uCount))
+	#print('Number of unknowns with unique lib strings counted: ' + str(uuCount))
+		#pprint.pprint(x)
+
+	#for x in still_unknown.maps:
+	#	if x.get('guessed_name') == 'unknown':
+	#		print('Not sure who ' + x.get('Filename') + ' is.')
+	#		uCount+=1
+	#print('Number of unknowns with unique lib strings counted: ' + str(uCount))
 
 
 if __name__ == "__main__":
