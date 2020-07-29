7221c6feca352c0683628f09abdaf8c854ae9efc
LeeLane <leeson10an@gmail.com>
2020-07-29 12:20:05 -0600

    Added two new entries for data structure along with new method for filling them. Creates two lists, one of the strings unique to the file and one of any strings that file has which is unique to the library. The plan is to use this to try and identify those files with strings which came back with no guesses after the string_Matching method was ran.

--- renameIT.py
+++ renameIT.py
@@ -1,4 +1,4 @@
-import os, subprocess, sys, argparse, re, collections
+import os, subprocess, sys, argparse, re, collections, pprint
 
 # Get a list of file names
 def get_fileNames(aDirectory):
@@ -50,7 +50,10 @@ def get_StringLists(aFileList, aDirectory):
 			"Filename": x,
 			"no_of_strings": len(strings),
 			"new_name" : "unknown",
-			"strings": strings
+			"guessed_name" : "unknown",
+			"strings": strings,
+			"unique_file_strings" : [],
+			"unique_lib_strings" : []
 			}
 	
 			#print(tmpDict["strings"][0])
@@ -63,8 +66,44 @@ def get_StringLists(aFileList, aDirectory):
 	
 	return hasStrings, noStrings, strings_chainMap
 
-# Try to find string matches
+# Use precense of unique library strings to make preliminary guesses
+def unique_StringFinder(unnamed_strings_chain, named_strings_chain):
+	tmpString = ""
+	total_unique_fStrings = []
 
+	# For each unnamed file extract the strings unique to that file
+	for x in unnamed_strings_chain.maps:
+		if x.get('Filename') != None:
+			fileStringList = x.get('strings')
+
+			# for each string in the file
+			for aString in fileStringList:
+				tmpString = tmpString + aString # concatenate together in order to use regex to pull out the strings
+
+			tmpList = re.findall('"([^"]*)"', tmpString) # pull out the strings from the file into a list
+			occurencesUnnamed = collections.Counter(tmpList) # count the occurences of each string in the file
+			tmpString = ""
+
+			# Check the occurences of the string in the file, if it is only once then add it to the list
+			# of unique file strings
+			for y in occurencesUnnamed:
+				if occurencesUnnamed[y] == 1:
+					z = x.get('unique_file_strings')
+					z.append(y) # append the string to the unique file strings list
+
+					if y not in total_unique_fStrings:
+						total_unique_fStrings.append(y) # append the string to the total unique file strings list
+						z = x.get('unique_lib_strings')
+						z.append(y)
+
+
+			#print(x.get('Filename') + ' unique_lib_strings: ')
+			#print(x.get('unique_lib_strings'))
+
+	return unnamed_strings_chain
+
+
+# Try to find file matches based off of string matches
 def string_Matching(unnamed_strings_chain, named_strings_chain):
 	i = 0
 	guesses = []
@@ -79,20 +118,33 @@ def string_Matching(unnamed_strings_chain, named_strings_chain):
 				tmpX = x.get('strings')
 				tmpZ = z.get('strings')
 
+				# if strings array of x is the same as string array of guess z
 				if tmpX == tmpZ:
-					print('Guess is that ' + x.get('Filename') + " is " + z.get('Filename'))
+					#print('Guess is that ' + x.get('Filename') + " is " + z.get('Filename'))
+					x.update(guessed_name = z.get('Filename'))
 					break
 
-					# send guesses to confirm method
-					
+				if x.get('no_of_strings') >= 2:
+					inList = 0
+					notInList = 0
+					for v in tmpX: # for each string in the file x string list
+						if v in tmpZ: # if that string is in the guess z file string list
+							#print('Maybe ' + x.get('Filename') + " is " + z.get('Filename') + " since '" + v + "' is in " + z.get('Filename'))
+							inList+=1
+						else:
+							notInList+=1
+					if inList > notInList:
+						#print('*Guess is that ' + x.get('Filename') + " is " + z.get('Filename'))
+						x.update(guessed_name = z.get('Filename'))
 
-			#print("Guesses for " + x.get('Filename'))
-			#print(guesses)
-
+	
 			# Reset list
-			#guesses = []
+			guesses = []
 
-def confirm_Matches(guesses):
+	return unnamed_strings_chain
+		
+
+#def confirm_Matches(guesses):
 	# TO-DO
 
 def main():
@@ -105,8 +157,15 @@ def main():
 	namedHasStrings, namedNoStrings, named_strings_chain = get_StringLists(namedFileList, 'instantcoffee_named')
 	unnamedHasStrings, unnamedNoStrings, unnamed_strings_chain = get_StringLists(unnamedFileList, 'instantcoffee_unnamed')
 	
-	string_Matching(unnamed_strings_chain, named_strings_chain)
-	#print(named_strings_chain.maps)
+	unnamed_stringUnique_chain = unique_StringFinder(unnamed_strings_chain, named_strings_chain)
+
+	
+	unnamed_stringMatches_chain = string_Matching(unnamed_stringUnique_chain, named_strings_chain)
+
+	pprint.pprint(unnamed_stringMatches_chain)
+
+	#for x in unnamed_stringUnique_chain.maps:
+	#	pprint.pprint(x)
 
 
 if __name__ == "__main__":
